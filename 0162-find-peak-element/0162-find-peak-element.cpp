bcabba318faea2be090243e7a3df449a391505c0
Samir Jout <jout.samir@proton.me>
2025-09-01 23:22:28 +0200

    Time: 0 ms (100%), Space: 12.3 MB (92.1%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
+    int findPeakElement(std::vector<int>& nums) {
         int left  = 0;
         int right = nums.size() - 1;
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[mid + 1]) {
-                right = mid;
+            if (mid > 0 && nums[mid] < nums[mid - 1]) {
+                right = mid - 1;
+            } else if (mid < nums.size() - 1 && nums[mid] < nums[mid + 1]) {
+                left = left + 1;
             } else {
-                left = mid + 1;
+                return mid;
             }
         }
-        return left;
+        return -1;
     }
 };
