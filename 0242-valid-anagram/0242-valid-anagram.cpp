b3654c55c06b6c7c4cec953b4c05bae17e2c1258
Samir Jout <jout.samir@proton.me>
2025-12-15 23:49:39 +0100

    0242-VALID-ANAGRAM Stats: Time: 1 ms (57.66%), Space: 10 MB (10.9%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        std::unordered_map<int, int> freqs;
-        for (const auto& chr : s)
-            freqs[chr] += 1;
-        for (const auto& chr : t)
-            freqs[chr] -= 1;
+    bool isAnagram(std::string s, std::string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+        std::unordered_map<char, int> freqs;
+        for (int i = 0; i < s.size(); ++i) {
+            freqs[s[i]] += 1;
+            freqs[t[i]] -= 1;
+        }
         for (const auto& [_, freq] : freqs) {
-            if (freq != 0)
+            if (freq != 0) {
                 return false;
+            }
         }
         return true;
     }
