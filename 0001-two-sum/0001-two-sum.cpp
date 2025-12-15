9411ee9e20eadb027980862156bc6d6f23226395
Samir Jout <jout.samir@proton.me>
2025-12-15 23:51:52 +0100

    0001-TWO-SUM Stats: Time: 0 ms (100%), Space: 14.8 MB (37.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        std::unordered_map<int, int> elements;
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> seen;
         for (int i = 0; i < nums.size(); ++i) {
-            int r = target - nums[i];
-            if (elements.find(r) != elements.end())
-                return { elements[r], i };
-            elements[nums[i]] = i;
+            const int r = target - nums[i];
+            if (seen.find(r) != seen.end()) {
+                return { seen[r], i };
+            }
+            seen[nums[i]] = i;
         }
         return {};
     }
