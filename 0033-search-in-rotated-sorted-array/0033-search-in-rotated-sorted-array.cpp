b528512299602afdedcfb1821c177ee45d24f66f
Samir Jout <jout.samir@proton.me>
2026-05-22 18:01:39 +0200

    0033-SEARCH-IN-ROTATED-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 15.2 MB (33.81%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,30 +1,37 @@
 class Solution {
-public:
-    int findMinimum(const std::vector<int>& nums) {
-        int left  = 0;
-        int right = nums.size() - 1; 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] <= nums[nums.size() - 1]) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+private:
+    int getPivot(const vector<int>& nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] < nums[r]) {
+                cout << "m = " << m << endl;
+                r = m;
+            }
+            else {
+                l = m + 1;
             }
         }
-        return left;
+        return r;
     }
+public:
     int search(vector<int>& nums, int target) {
-        int inflection = findMinimum(nums);
-        int left       = target <= nums[nums.size() - 1] ? inflection : 0;
-        int right      = target <= nums[nums.size() - 1] ? nums.size() - 1: std::max(0, inflection - 1);
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+        int pivot = getPivot(nums);
+        int l     = (target > nums[nums.size() - 1] ? 0 : pivot);
+        int r     = (target > nums[nums.size() - 1] ? pivot - 1 : nums.size() - 1);
+        cout << pivot << endl;
+        cout << l << "  " << r << endl;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return m;
+            }
+            if (nums[m] > target) {
+                r = m - 1;
+            }
+            else {
+                l = m + 1;
             }
         }
         return -1;
