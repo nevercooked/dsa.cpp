983831123d3252165ee751d7c0f084cfbdfe6d24
Samir Jout <jout.samir@proton.me>
2026-06-22 01:19:44 +0200

    0576-OUT-OF-BOUNDARY-PATHS Stats: Time: 7 ms (52.1%), Space: 12.1 MB (39.85%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -1,4 +1,6 @@
 class Solution {
+private:
+    static constexpr int MOD = 1e9 + 7;
 public:
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
         vector<pair<int, int>> directions = {
@@ -12,22 +14,18 @@ public:
             if (i < 0 || i >= m || j < 0 || j >= n) {
                 return 1;
             }
-            if (moves == maxMove) {
-                return 0;
-            }
             if (dp[i][j][moves] != -1) {
                 return dp[i][j][moves];
             }
             int paths = 0;
-            int mod   = 1e9 + 7;
             for (auto [x, y] : directions) {
                 x += i;
                 y += j;
                 if (moves + 1 <= maxMove) {
-                    paths = (paths + self(x, y, moves + 1)) % mod;
+                    paths = (paths + self(x, y, moves + 1)) % MOD;
                 }
             }
-            return (dp[i][j][moves] = paths) % mod;
+            return (dp[i][j][moves] = paths) % MOD;
         };
         return dfs(startRow, startColumn, 0);
     }
