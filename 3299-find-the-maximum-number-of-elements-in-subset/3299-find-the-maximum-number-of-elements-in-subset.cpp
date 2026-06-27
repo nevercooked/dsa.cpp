e356dc68990bbca205f1e8c4712f3d013cce1a0f
Samir Jout <jout.samir@proton.me>
2026-06-27 20:50:01 +0200

    3299-FIND-THE-MAXIMUM-NUMBER-OF-ELEMENTS-IN-SUBSET Stats: Time: 540 ms (5.14%), Space: 118.8 MB (80.57%) - LeetHub

--- 3299-find-the-maximum-number-of-elements-in-subset/3299-find-the-maximum-number-of-elements-in-subset.cpp
+++ 3299-find-the-maximum-number-of-elements-in-subset/3299-find-the-maximum-number-of-elements-in-subset.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        unordered_map<long long, int> freqs;
-        int                           n      = nums.size();
-        int                           maxLen = 0;
+        unordered_map<int, int> freqs;
         for (const auto& num : nums) {
             freqs[num] += 1;
         }
-        maxLen = (freqs[1] % 2 == 0 ? freqs[1] - 1 : freqs[1]);
+        int maxLen = (freqs[1] % 2 == 0 ? freqs[1] - 1 : freqs[1]); 
         freqs.erase(1);
         for (const auto& [num, _] : freqs) {
-            int       currLen = 0;
-            long long x       = num;
-            for (; freqs.find(x) != freqs.end() && freqs[x] > 1; x *= x) {
+            int currLen = 0;
+            int x       = num;
+            for (; freqs.find(x) != freqs.end() && freqs[x] > 1 && std::numeric_limits<int>::max() / x >= x; x *= x) {
                 currLen += 2;
             }
-            maxLen = max(
-                maxLen,
-                currLen + (freqs.find(x) != freqs.end()  && freqs[x] > 0 ? 1 : -1)
-            );
+            cout << x << endl;
+            currLen += (freqs.find(x) != freqs.end() && freqs[x] > 0 ? 1 : -1); 
+            maxLen   = max(maxLen, currLen);
         }
         return maxLen;
     }
