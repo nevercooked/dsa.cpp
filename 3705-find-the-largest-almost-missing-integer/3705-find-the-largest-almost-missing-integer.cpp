5ea2632e26581f6857e075ce8764fdf6b6de0056
Samir Jout <jout.samir@proton.me>
2026-08-18 23:15:50 +0200

    3705-FIND-THE-LARGEST-ALMOST-MISSING-INTEGER Stats: Time: 4 ms (46.18%), Space: 29.2 MB (66.55%) - LeetHub

--- 3705-find-the-largest-almost-missing-integer/3705-find-the-largest-almost-missing-integer.cpp
+++ 3705-find-the-largest-almost-missing-integer/3705-find-the-largest-almost-missing-integer.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
+        if (k == nums.size()) return *max_element(nums.begin(), nums.end());
         unordered_map<int, int> freqs;
-        for (const auto& num : nums) {
-            freqs[num] += 1;
-        }
-        int largest = -1;
-        if (k == nums.size()) {
-            largest = *max_element(nums.begin(), nums.end());
-        }
-        else if (k == 1) {
-            for (const auto& num : nums) {
-                if (freqs[num] == 1) {
-                    largest = max(largest, num);
-                }
-            }
+        int                     largest = -1;
+        for (const auto& num : nums) freqs[num] += 1;
+        if (k == 1) {
+            for (const auto& num : nums) if (freqs[num] == 1) largest = max(largest, num);
         }
         else {
-            if (freqs[nums[0]] == 1) {
-                largest = nums[0];
-            }
-            if (freqs[nums[nums.size() - 1]] == 1) {
-                largest = max(largest, nums[nums.size() - 1]);
-            }
+            if (freqs[nums[0]] == 1)               largest = nums[0];
+            if (freqs[nums[nums.size() - 1]] == 1) largest = max(largest, nums[nums.size() - 1]);
         }
         return largest;
     }
