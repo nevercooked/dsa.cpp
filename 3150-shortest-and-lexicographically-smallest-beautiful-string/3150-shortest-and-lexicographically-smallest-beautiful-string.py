35ea4548de804ed9460dc028bc963cc2a2a42de8
Samir Jout <jout.samir@proton.me>
2026-08-26 22:16:17 +0200

    3150-SHORTEST-AND-LEXICOGRAPHICALLY-SMALLEST-BEAUTIFUL-STRING Stats: Time: 0 ms (100%), Space: 19.4 MB (30.63%) - LeetHub

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/3150-shortest-and-lexicographically-smallest-beautiful-string.py
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/3150-shortest-and-lexicographically-smallest-beautiful-string.py
@@ -6,15 +6,13 @@ class Solution:
         ones = 0
         i = 0
         for j, ch in enumerate(s):
-            if ch == '1':
-                ones += 1
+            ones += int(ch)
             while i <= j and (ones > k or s[i] == '0'):
-                if s[i] == '1':
-                    ones -= 1
+                ones -= int(s[i])
                 i += 1
             if ones == k:
-                t = s[i:j + 1]
-                if len(t) < len(shortest) or (len(t) == len(shortest) and t < shortest):
-                    shortest = t
+                ss = s[i:j + 1]
+                if len(ss) < len(shortest) or (len(ss) == len(shortest) and ss < shortest):
+                    shortest = ss
         return shortest
             
